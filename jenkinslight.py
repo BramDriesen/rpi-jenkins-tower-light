e876a6c5cd54901761cc03a8429ad6890e95d71f
bdriesen <bram.driesen@capgemini.com>
2016-04-06 21:10:16 +0200

    just some things

---
+++ jenkinslight.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# Program flow
+# 1. On startup: beep twice and blink leds one by one.
+# 2. Check if connection possible with Jenkins. If not blink red.
+# 3. Get the status and display the color.
+
+# import RPi.GPIO as GPIO
+import time
+
+time.sleep(1)
+# beep
+time.sleep(1)
+# beep
