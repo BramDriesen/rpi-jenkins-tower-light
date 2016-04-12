ad5f0288094e3d6d756976b18ebed8957a6887a4
BramDriesen <bramdriesen94@gmail.com>
2016-04-12 21:01:00 +0000

    basic testing

--- jenkinslight.py
+++ jenkinslight.py
@@ -5,10 +5,28 @@
 # 2. Check if connection possible with Jenkins. If not blink red.
 # 3. Get the status and display the color.
 
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
+GPIO.setmode(GPIO.BCM)
 
-time.sleep(1)
-# beep
-time.sleep(1)
-# beep
+# GPIO Setup
+# 18 = Red
+# 23 = Buzzer
+# 24 = Yellow
+# 27 = Green
+GPIO.setup(18, GPIO.OUT)
+GPIO.setup(23, GPIO.OUT)
+GPIO.setup(24, GPIO.OUT)
+GPIO.setup(27, GPIO.OUT)
+
+# Always set all outputs to false
+GPIO.output(18, False)
+GPIO.output(23, False)
+GPIO.output(24, False)
+GPIO.output(27, False)
+
+
+time.sleep(5)
+GPIO.output(27, True)
+time.sleep(5)
+GPIO.output(27, False)
