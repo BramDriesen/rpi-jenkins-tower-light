ce8bea24f245b326c6e80afcf13c46793adb3b21
BramDriesen <bramdriesen94@gmail.com>
2016-04-17 23:13:26 +0000

    Debugged and fixed some warnings. Also added support to exit the script and catching connection erros with the jenkinsapi package

--- jenkinslight.py
+++ jenkinslight.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python
 
-# Program flow
-# 1. On startup: beep and blink leds one by one.
-# 2. Check if connection possible with Jenkins. If not blink red. (TODO)
-# 3. Get the status and display the color.
-# 4. Check with threads if building or not / update status lught
-
 import RPi.GPIO as GPIO
 import time
 import threading
 import jenkinsapi
-from jenkinsapi.jenkins import Jenkins
 import config as cfg
+import signal
+from jenkinsapi.jenkins import Jenkins
+from requests.exceptions import ConnectionError
 
 # Set the GPIO mode.
 GPIO.setmode(GPIO.BCM)
@@ -28,14 +24,17 @@ def getcode(value):
     }.get(value, 24)
 
 # GPIO Setup
+GPIO.setwarnings(False)
 GPIO.setup(getcode('red'), GPIO.OUT)
 GPIO.setup(getcode('buzzer'), GPIO.OUT)
 GPIO.setup(getcode('yellow'), GPIO.OUT)
 GPIO.setup(getcode('green'), GPIO.OUT)
 
+
 # Global variables
 building = False
 error = False
+keepalive = True
 
 # ---------------------------------------------------- #
 # --         All globally used functions            -- #
@@ -72,74 +71,87 @@ def setstatus(build):
             GPIO.output(getcode('red'), True)
     return
 
+def setError(value):
+    global error
+    if value == True or value == False:
+        error = value
+    else:
+        error = True
+        print "[ERROR] Only supply True or False to the setError function"
+
 # ---------------------------------------------------- #
 
 # Turn all off if any GPIO is still on
 alloff()
 
 # Toggle everything once
-toggle(getcode('buzzer'), .1)
+# toggle(getcode('buzzer'), .1)
 toggle(getcode('red'), .2)
 toggle(getcode('yellow'), .2)
 toggle(getcode('green'), .2)
 
 # Configure the Jenkins parameter and get the job
-J = Jenkins(cfg.jenkinsurl, username=cfg.username, password=cfg.password)
-
 try:
-    job = J.get_job("UTC Behat")
+    J = Jenkins(cfg.jenkinsurl, username=cfg.username, password=cfg.password)
+except jenkinsapi.custom_exceptions.JenkinsAPIException:
+    setError(True)
+    print "[ERROR] Jenkins authentication error!"
+except ConnectionError:
+    setError(True)
+    print "[ERROR] Jenkins connection error!"
+else:
+    print "[INFO] No Jenkins error!"
+    setError(False)
     # Get the status of the latest build before starting the threads
+    job = J.get_job("UTC Behat")
     latestbuild = job.get_last_build()
     setstatus(latestbuild)
-except IOError:
-    global error
-    error = True
-    print "IOERROR"
-except ValueError:
-    global error
-    error = True
-    print "Value Error"
-except:
-    global error
-    error = True
-    print "Could not connect to Jenkins, please check your internet connection and settings"
-else:
-    global error
-    print "No error?"
-    error = False
 
 # Thread the blinking function
 # Every 10s blink for 3s when we are building
 def blinking():
-    threading.Timer(10.0, blinking).start()
-    # Only blink when we are actually building
-    if building == True or error == True:
-        # If error, blink red.
-        if error == True:
-            color = "red"
-        else:
-            color = "yellow"
+    if keepalive == True:
+        threading.Timer(10.0, blinking).start()
 
-        alloff()
-        pin = getcode(color)
+        # Only blink when we are actually building
+        if building == True or error == True:
+            # If error, blink red.
+            if error == True:
+                color = "red"
+            else:
+                color = "yellow"
 
-        GPIO.output(pin, True)
-        time.sleep(3)
-        GPIO.output(pin, False)
+            alloff()
+            pin = getcode(color)
+            GPIO.output(pin, True)
+            time.sleep(3)
+            GPIO.output(pin, False)
 
 # Check every 10s if we are building, if not or done get latest status     
 def buildrunning():
-    threading.Timer(10.0, buildrunning).start()
-    
-    if job.is_running() == True:
-        global building
-        building = True
-    else:
-        global building
-        building = False
-        setstatus(job.get_last_build())
+    if keepalive == True:
+        threading.Timer(10.0, buildrunning).start()
+        if error == False:
+            global building
+            if job.is_running() == True:
+                building = True
+            else:
+                building = False
+                setstatus(job.get_last_build())
 
 # Initiate the threads
 blinking()
 if error == False:
     buildrunning()
+
+# Catch the keyboard intterupt and stop the threads from executing
+def main():
+    global keepalive
+    while keepalive == True:
+        try:
+            time.sleep(1)
+        except KeyboardInterrupt:
+            print "[NOTICE] Jenkinslight terminated!"
+            alloff()
+            keepalive = False
+main()
