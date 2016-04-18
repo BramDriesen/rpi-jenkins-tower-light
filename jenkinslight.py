d876e6805e76e69a0578cabf2927e21b0b4e399f
bdriesen <bram.driesen@capgemini.com>
2016-04-18 23:53:24 +0200

    set status according to build results

--- jenkinslight.py
+++ jenkinslight.py
@@ -98,9 +98,12 @@ def checkJobs():
             if getstatus == "FAILURE":
                 failed += 1
 
-            print getstatus
-
-
+            if success > 0 and unstable == 0 & failed == 0:
+                setstatus('SUCCESS')
+            if unstable > 0 & failed == 0:
+                setstatus('UNSTABLE')
+            if failed > 0:
+                setstatus('FAILURE')
 
 # ---------------------------------------------------- #
 
