dd352a4db1e1b9fff1b39ff0b716481d3208ce5e
bdriesen <bram.driesen@capgemini.com>
2017-08-14 21:37:31 +0200

    Added test file for the automation hat

---
+++ test_hat.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import time
+
+import automationhat
+
+if automationhat.is_automation_hat():
+    automationhat.light.power.write(1)
+
+while True:
+    if automationhat.is_automation_hat():
+        automationhat.light.comms.toggle()
+        automationhat.light.warn.toggle()
+
+    automationhat.output.one.toggle()
+
+    print(automationhat.analog.read())
+
+    time.sleep(2)
